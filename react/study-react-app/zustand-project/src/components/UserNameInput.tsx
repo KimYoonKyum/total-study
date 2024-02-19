916fcdb9d99ecbec78b0834acc06c88a31a3fb96
kim yoonkyum <newalpha7@naver.com>
2024-02-19 09:56:51 +0900

    jebratin ai 사용

--- react/study-react-app/zustand-project/src/components/UserNameInput.tsx
+++ react/study-react-app/zustand-project/src/components/UserNameInput.tsx
@@ -1,36 +1,43 @@
-import { ChangeEvent } from "react";
+import {ChangeEvent} from "react";
 import userStore from "../store/UserStore.ts";
 
+interface UserInputFieldProps {
+    value: string;
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    placeholder: string;
+}
+
+export function UserInputField({value, onChange, placeholder}: UserInputFieldProps) {
+    return (
+        <input type="text" value={value} onChange={onChange} placeholder={placeholder}/>
+    );
+}
+
 export default function UserNameInput() {
-  const firstName = userStore((state) => state.firstName);
-  const lastName = userStore((state) => state.lastName);
-  const setFirstName = userStore((state) => state.setFirstName);
-  const setLastName = userStore((state) => state.setLastName);
-  const onChangeFirstName = (e: ChangeEvent<HTMLInputElement>) => {
-    setFirstName(e.target.value);
-  };
+    const firstName = userStore((state) => state.firstName);
+    const lastName = userStore((state) => state.lastName);
+    const setFirstName = userStore((state) => state.setFirstName);
+    const setLastName = userStore((state) => state.setLastName);
 
-  const onChangeLastName = (e: ChangeEvent<HTMLInputElement>) => {
-    setLastName(e.target.value);
-  };
+    const handleInputChange = (setter: (value: string) => void) => (event: ChangeEvent<HTMLInputElement>) => {
+        setter(event.target.value);
+    };
 
-  return (
-    <div>
-      <div>
-        <input
-          type={"text"}
-          value={firstName}
-          onChange={onChangeFirstName}
-          placeholder={"first name"}
-        />
-        <input
-          type={"text"}
-          value={lastName}
-          onChange={onChangeLastName}
-          placeholder={"last name"}
-        />
-      </div>
-      <div>{`${firstName} ${lastName}`}</div>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <UserInputField
+                    value={firstName}
+                    onChange={handleInputChange(setFirstName)}
+                    placeholder="first name"
+                />
+                <UserInputField
+                    value={lastName}
+                    onChange={handleInputChange(setLastName)}
+                    placeholder="last name"
+                />
+            </div>
+            <div>{`${firstName} ${lastName}`}</div>
+        </div>
+    );
 }
